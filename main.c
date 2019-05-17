79f24142ed9ec0fcde98c8669b1260aabaed2d0e
CLHJSWE <2291237307@qq.com>
2019-05-17 09:39:20 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int judge(int a[][16],int i,int j)
+{
+	int i,jb[4][4];
+	for(i=0;i<4;i++)
+		for(j=0;j<4;j++)
+			b[i][j]=a[i]
+	
+	return 0;
+}
+
+
+int main()
+{
+	int T,i,j,count;
+	char a[20][16]; 
+	
+	scanf("%d%c",&T);
+	
+	for(i=0;i<T;i++)
+		for(j=0;j<16;j++)
+			scanf("%c",*(a+i)+j);
+	
+	for(i=0;i<T;i++)
+	{
+		count=0;
+		for(j=0;j<16;j++)
+		{
+			printf("%c ",a[i][15]);
+			if(a[i][j]=='.' && judge(a,i,j)==0)
+				count++;
+		}
+		printf("%d\n",count);
+	}
+	
+	return 0;
+}
